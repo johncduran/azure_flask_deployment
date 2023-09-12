db1b19b85c4616097f636ca11b0edbc9f7c674f2
johncduran <john.duran@stonybrook.edu>
2023-09-12 01:41:31 +0000

    added more work

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template
 import pandas as pd
+import random
+from faker import Faker
+
+fake = Faker()
 
 app = Flask(__name__)
 
 @app.route('/')
 def mainpage():
     return render_template('base.html')
+
+@app.route('/about')
+def aboutpage():
+    return render_template('about.html')
+
+@app.route('/random')
+def randomnumber():
+    number_var = random.randint(1,10000)
+    fake_address = Faker.address
+    return render_template('random.html', single_number = number_var, single_address = fake_address)
 
 
 if __name__ == '__main__':
